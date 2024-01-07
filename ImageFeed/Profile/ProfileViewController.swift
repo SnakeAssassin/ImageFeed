bcd64f8ac6a63f1f13d6af595b0eef1838dba38f
Tseren Ubushaev <snakeassassin@icloud.com>
2024-01-07 19:14:35 +0300

    Сверстан экран ProfileViewController

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet private var profileImageView: UIImageView!
+    
+    @IBOutlet private var logoutButton: UIButton!
+    
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var idLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+    
+    @IBAction private func didTapLogoutButton() {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
